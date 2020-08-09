7b5cd8c9e96e2944103e506f6229c8b58f10cc05
Adi1169 <adityahfjc17@gmail.com>
2020-08-09 18:02:36 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'font-awesome/css/font-awesome.css';
-import 'bootstrap-social/bootstrap-social.css'; 
+import 'bootstrap-social/bootstrap-social.css';
+import {BrowserRouter} from 'react-router-dom';
 import Main from './components/MainComponent';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
