e71ad2e644c3073a6eb39783ee3a2b9d9eefd456
Adi1169 <adityahfjc17@gmail.com>
2020-08-16 10:41:49 +0530

    third

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle ,BreadcrumbItem, Breadcrumb} from 'reactstrap';
+import {Link} from 'react-router-dom'
 function RenderDish({dish}){
     return(
         <Card>
@@ -12,9 +13,9 @@ function RenderDish({dish}){
         </Card>
      );
 }
-function RenderComments({dish}){
+function RenderComments({comments}){
     
-    const comments=dish.comments.map((comment)=>{
+    const commentItem=comments.map((comment)=>{
         const months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         const year=comment.date.substring(0,4);
         const month=comment.date.substring(5,7);
@@ -28,20 +29,31 @@ function RenderComments({dish}){
             </div>
         );
     });
-    console.log(comments);
-    return comments;
+    console.log(commentItem);
+    return commentItem;
 }
     const DishDetailComponent =(props) =>{
         if(props.dish!=null){
             return(
                 <div className="container">
                     <div className="row">
+                        <Breadcrumb>
+
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
                         <div  className="col-12 col-md-5 m-1">
                             <RenderDish dish={props.dish}/>
                         </div>
                         <div className="col-12 col-md-5 m-1">
                             <h4>Comments</h4>
-                            <RenderComments dish={props.dish}/>
+                            <RenderComments comments={props.comments}/>
                         </div>
                     </div>
                 </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
+import About from './AboutUs';
 import {Switch,Route,Redirect} from 'react-router-dom';
 class Main extends Component {
 
@@ -12,6 +17,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments:COMMENTS,
+        promotions:PROMOTIONS,
+        leaders:LEADERS
     };
   }
 
@@ -19,7 +27,22 @@ class Main extends Component {
   render() {
     const HomePage=() =>{
       return(
-        <Home/>
+        <Home 
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    }
+    const DishWithId = ({match})=>{
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    );
+    }
+    const AboutUs= ()=>{
+      return(
+        <About leaders={this.state.leaders}/>
       );
     }
     return (
@@ -27,7 +50,10 @@ class Main extends Component {
         <Header/>
         <Switch>
           <Route path="/home" component={HomePage}/>
+          <Route exact  path="/aboutus" component={AboutUs}/>
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+          <Route path="/menu/:dishId" component={DishWithId}/>
+          <Route exact path="/contactus" component={Contact}/>
           <Redirect to="/home" />
         </Switch>
         <Footer/>
